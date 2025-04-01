46d7a5cd52383ffc0367afba5cc27734e35f7c49
Tony901080 <dangthetony1707@gmail.com>
2025-04-01 22:18:47 +0700

    Update 3

---
+++ LoginUser.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "User_Auth.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Hàm đăng nhập trả về UserNode*
+UserNode* login_User(UserNode* root, const string& username, const string& password);
+
+// Hàm đăng nhập có giới hạn số lần thử
+void loginProcess(UserNode* root);
+
+// Hàm bắt đầu trò chơi
+void newGame();
