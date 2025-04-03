b0fab7a68f6d9571640bcecad56d78ee367b3460
Nguyen Nam <namnqs12@gmail.com>
2025-04-03 19:40:37 +0700

    Renamed

---
+++ Project_CSC101/Menu.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <functional>
+#include "User_Auth.h"
+#include "Register.h"
+#include "LoginUser.h"
+#include "Game_function.h"
+#include "LeaderBoard.h"
+#include "Resume.h"
+#include <conio.h> // Thư viện cho getch()
+
+using namespace std;
+
+// Chơi game 2048
+void playGame();
+// Hiển thị menu chính
+void mainMenu();
+// Hàm thoát game
+void exitGame();
